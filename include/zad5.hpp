d800bdd2c7d0e04ace538cb438f80f3b3edd2617
wtzmuda <wtzmuda@gmail.com>
2025-12-12 22:30:12 +0100

    zad5 v1

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -6,4 +6,11 @@
 #include <random>
 #include <vector>
 
-// tutaj funkcja sortujTagliatelle
+template<typename Iterator>
+void sortujTagliatelle(Iterator begin, Iterator end)
+{
+    std::sort(begin, end, [](const tagiatelle& a, const tagiatelle& b)
+        {
+        return a.ileMaki(1) < b.ileMaki(1);
+    });
+}
