f552118fa47cb6c972fe074ae78e5b303e74533a
wtzmuda <wtzmuda@gmail.com>
2025-12-12 22:28:16 +0100

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,22 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+
+
+class tagiatelle : public Makaron
+{
+private:
+    double L; // długość kawałka makaronu
+    double W; // szerokość kawałka makaronu
+    double R; // proporcja jajek do mąki
+
+    static double C;
+
+public:
+    tagiatelle(double l, double w, double r) : L(l), W(w), R(r) {};
+    nitka() : L(0.5), W(0.5), R(0.5) {};
+
+    double ileMaki(unsigned P) const override { return P * L * W * (1. - R) * C;
+    }
+
+}
