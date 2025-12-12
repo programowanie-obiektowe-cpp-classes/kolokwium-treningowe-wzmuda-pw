74bb4d6138f390c1aa54622a4d7ae7afca4e246f
wtzmuda <wtzmuda@gmail.com>
2025-12-12 22:29:14 +0100

    zad3 v1

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -2,4 +2,8 @@
 #include <cstdint>
 #include <functional>
 
-// tutaj funkcja polejSosem
+template<typename SosTyp>
+std::size_t polejSosem(const tagiatelle& makaron, const SosTyp& sos)
+{
+    return sos.polej(makaron);
+}
