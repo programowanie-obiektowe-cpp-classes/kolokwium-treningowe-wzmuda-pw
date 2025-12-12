fb2f6bab0cdaed7fed882db37ded86a3da6c12e1
wtzmuda <wtzmuda@gmail.com>
2025-12-12 22:28:52 +0100

    zad2 v1

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,6 +4,42 @@
 #include <string>
 #include <type_traits>
 
+class Makaron
+{
+public:
+
+
+    virtual ~Makaron()                     = default;
+
+    virtual double ileMaki(unsigned P) const = 0;
+
+    static Makaron* gotujMakaron(const std::string& s);
+
+};
+
 // tutaj klasa Penne
 
+class Penne : public Makaron
+{
+public:
+    double ileMaki(unsigned P) const override { return static_cast< double >(P); };
+};
 // tutaj definicja metody gotujMakaron
+
+Makaron* Makaron::gotujMakaron(const std::string& s)
+{
+
+    if (s.empty())
+    {
+        return new Penne();
+    }
+
+    if (s.front() == s.back())
+    {
+        return new tagiatelle(3.14, 0.42, 0.1);
+    }
+    else
+    {
+        return new Penne();
+    }
+}
