ba34bee0f0e59a95393f16f7319e68ac31906ee6
wtzmuda <wtzmuda@gmail.com>
2025-12-12 22:29:43 +0100

    zad4 v1

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -2,4 +2,24 @@
 
 #include <vector>
 
-// tutaj funkcja obliczMake
+double obliczakMake(const std::vector< tagiatelle >& Makaron)
+{
+    double M = 0.0;
+    unsigned mnoznik = 1;
+    for (auto it = Makarony.rbegin(); it != Makarony.rend(); ++it)
+    {
+        M += Makaron->ileMaki(mnoznik);
+        ++mnoznik;
+    }   
+
+    if (M>100.0)
+    {
+        throw 404;
+    }
+    else if (M> 50.0)
+    {
+        throw 3.14;
+    }
+    return M;
+
+   }
